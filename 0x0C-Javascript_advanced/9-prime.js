81cd6df7d0f5b130782dce97b28857e7d7a6a3b1
jashjchoi <jashjchoi@gmail.com>
2021-09-22 04:53:09 +0000

    add task 10 and fix the syntax

--- 0x0C-Javascript_advanced/9-prime.js
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -1,25 +1,25 @@
-function countPrimeNumbers() {
-    let primes = [2];
-    for (let i = 2; i <= 100; i++ ) {
-        if (i % 2 !== 0) {
-            let isPrime = true;
-            for (let prime  of primes) {
-                if (i % prime === 0) {
-                    isPrime = false;
-                    break;
-                };
-            };
-            if (isPrime) {
-                primes.push(i);
-            };
-        };
-    };
-    return(primes.length);
-};
+function countPrimeNumbers () {
+  const primes = [2];
+  for (let i = 2; i <= 100; i++) {
+    if (i % 2 !== 0) {
+      let isPrime = true;
+      for (const prime of primes) {
+        if (i % prime === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        primes.push(i);
+      }
+    }
+  }
+  return (primes.length);
+}
 console.log(countPrimeNumbers());
 
-let t0 = performance.now();
+const t0 = performance.now();
 countPrimeNumbers();
-let t1 = performance.now();
+const t1 = performance.now();
 
 console.log(`Execution time of printing countPrimeNumbers was ${t1 - t0} milliseconds.`);
